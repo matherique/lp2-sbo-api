b5082220e02e8b182916eb9a382646c4a6d4c5b7
Matheus Henrique <matherique@gmail.com>
2022-01-09 01:03:03 -0300

    use env variables

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -7,23 +7,35 @@ import (
 	"os"
 	"os/signal"
 
+	"github.com/matherique/lp2-sbo-api/pkg/config"
 	server "github.com/matherique/lp2-sbo-api/pkg/http_server"
 	"github.com/matherique/lp2-sbo-api/pkg/store"
 )
 
-const port = ":5000"
-
 func main() {
 	mux := http.NewServeMux()
 
-	store := store.NewStore()
+	config, err := config.Read()
+
+	if err != nil {
+		log.Fatalf("could not read config: %v", err)
+	}
+
+	store := store.NewStore(
+		config.DATABASE_HOST,
+		config.DATABASE_PORT,
+		config.DATABASE_DBNAME,
+		config.DATABASE_USERNAME,
+		config.DATABASE_PASSWORD,
+	)
+
 	if err := store.Connect(); err != nil {
 		log.Fatal(err)
 	}
 
 	loadRoutes(mux, store)
 
-	srv := server.NewServer(port, mux)
+	srv := server.NewServer(config.APP_PORT, mux)
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt)
 
